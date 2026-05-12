fde6b475a11f32df564b4370d025605f13c3d0e4
Vidit pal <100254789+viditpal@users.noreply.github.com>
2026-05-12 18:46:01 +0530

    Add files via upload

---
+++ Automation_Maven/src/test/java/testing/Testdemo.java
@@ -0,0 +1,14 @@
+package testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+public class Testdemo {
+	
+	@Test
+	public void launchurl() {
+		WebDriver driver = new EdgeDriver();
+	}
+
+}
